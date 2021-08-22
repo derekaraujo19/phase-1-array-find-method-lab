cb4b5581e19a8902e8492950bc58f3779ddb9c32
Derek Araujo <derekaraujo19@gmail.com>
2021-08-22 17:53:32 -0400

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 
+// function superbowlWin(arrayObj) {
+//   if (arrayObj.result === "W") {
+//     return arrayObj.year;
+//   } else {
+//     return undefined;
+//   }
+// }
+
+// record.find(superbowlWin);
+
+
+// let superbowlWin = record.find(function(element) {
+//   return record.result === 'W';
+// })
+
+
+// function superbowlWin(array) {
+//   return array.result === 'W'
+// }
+
+// record.find(superbowlWin);
+
+
+function superbowlWin(record) {
+  let win = record.find(record => record.result === 'W');
+  if (win === undefined) {
+    return undefined;
+  } else {
+    return win.year;
+  }
+}
