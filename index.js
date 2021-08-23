29d4e41729adee3df7aa8683a7c794190571f008
Derek Araujo <derekaraujo19@gmail.com>
2021-08-23 13:53:19 -0400

    commit tree beb47291946e87a8112f4491b7061f4b8c2bba26

--- index.js
+++ index.js
@@ -23,7 +23,7 @@
 
 
 function superbowlWin(record) {
-  let win = record.find(record => record.result === 'W');
+  let win = record.find(element => element.result === 'W');
   if (win === undefined) {
     return undefined;
   } else {
